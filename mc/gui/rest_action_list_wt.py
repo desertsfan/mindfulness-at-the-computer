8e77597bf84359d373cf7cb28328b5d82f8cfd12
Tord <tord.dellsen@gmail.com>
2018-01-17 00:42:32 +0100

    Bug fix for when clicking move up/down for rest actions
    Please note that for the breathing phrases there's always a phrase selected

--- mc/gui/rest_action_list_wt.py
+++ mc/gui/rest_action_list_wt.py
@@ -83,6 +83,8 @@ class RestActionListWt(QtWidgets.QWidget):
         self.move_up_down(model.MoveDirectionEnum.down)
 
     def move_up_down(self, i_up_down: model.MoveDirectionEnum):
+        if mc_global.active_rest_action_id_it == mc_global.NO_REST_ACTION_SELECTED_INT:
+            return
         id_int = mc_global.active_rest_action_id_it
         model.RestActionsM.update_sort_order_move_up_down(id_int, i_up_down)
         self.update_gui()
